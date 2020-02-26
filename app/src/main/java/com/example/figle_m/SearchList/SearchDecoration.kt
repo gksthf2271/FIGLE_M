ffd7fb5349e0a409f53fb87948d03330b4cbf334
gksthf2271 <rlagksthf33@naver.com>
2020-02-27 00:10:03 +0900

    RecyclerView CustomDecoration 추가

---
+++ app/src/main/java/com/example/figle_m/SearchList/SearchDecoration.kt
@@ -0,0 +1,23 @@
+package com.example.figle_m.SearchList
+
+import android.graphics.Rect
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class SearchDecoration(divHeight: Int) : RecyclerView.ItemDecoration() {
+    var mDivHeight:Int
+    init{
+        mDivHeight = divHeight
+    }
+
+    override fun getItemOffsets(
+        outRect: Rect,
+        view: View,
+        parent: RecyclerView,
+        state: RecyclerView.State
+    ) {
+        super.getItemOffsets(outRect, view, parent, state)
+        outRect.top = mDivHeight
+    }
+
+}
