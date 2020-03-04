fee45a69f139a1d3896c78540a06289d3f8812ad
gksthf2271 <rlagksthf33@naver.com>
2020-03-05 00:30:24 +0900

    누락 소스

--- app/src/main/java/com/example/figle_m/SearchList/SearchPresenter.kt
+++ app/src/main/java/com/example/figle_m/SearchList/SearchPresenter.kt
@@ -11,6 +11,8 @@ class SearchPresenter: SearchContract.Presenter {
     val TAG:String = javaClass.name
     var  mSearchListView: SearchContract.View? = null
 
+    open val ERROR_EMPTY = "EMPTY"
+
     override fun takeView(view: SearchContract.View) {
         mSearchListView = view
     }
@@ -28,6 +30,7 @@ class SearchPresenter: SearchContract.Presenter {
                         mSearchListView?.showSearchList(it)
                 }, {
                     Log.v(TAG, "Result : getMatchDetailList response : $it")
+                    mSearchListView?.showError(ERROR_EMPTY)
                 })
 
             }
@@ -37,10 +40,10 @@ class SearchPresenter: SearchContract.Presenter {
     fun getMatchDetail(matchId: String, onSuccess: ((MatchDetailResponse) -> Unit), onFailed: (String) -> Unit) {
         DataManager.getInstance().loadMatchDetail(matchId,
             {
-                Log.v(TAG,"requestSuccess! ${it.matchId} + ${it.matchDate}")
+                Log.v(TAG,"getMatchDetail Success! ${it.matchId} + ${it.matchDate}")
                 onSuccess(it)
             }, {
-                Log.v(TAG,"requestFailed! $it")
+                Log.v(TAG,"getMatchDetail Failed! $it")
                 onFailed(it)
             })
     }
