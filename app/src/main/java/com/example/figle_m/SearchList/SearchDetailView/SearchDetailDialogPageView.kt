b10409c199c245ad13681169206e6fccda3ee6a0
gksthf2271 <rlagksthf33@naver.com>
2020-03-15 13:09:34 +0900

    detail 포맷 변경 및 cardView 추가

--- app/src/main/java/com/example/figle_m/SearchList/SearchDetailView/SearchDetailDialogPageView.kt
+++ app/src/main/java/com/example/figle_m/SearchList/SearchDetailView/SearchDetailDialogPageView.kt
@@ -32,7 +32,7 @@ class SearchDetailDialogPageView : ConstraintLayout {
         val possessionView = findViewById<SearchDetailItemView>(R.id.cview_item1)
         val foulView = findViewById<SearchDetailItemView>(R.id.cview_item2)
         val cornerKickView = findViewById<SearchDetailItemView>(R.id.cview_item3)
-        val cardView = findViewById<SearchDetailItemView>(R.id.cview_item4)
+        val cardView = findViewById<SearchDetailItemCardView>(R.id.cview_item4)
         val passView = findViewById<SearchDetailItemView>(R.id.cview_item5)
         val goalView = findViewById<SearchDetailItemView>(R.id.cview_item6)
         val defenceView = findViewById<SearchDetailItemView>(R.id.cview_item7)
@@ -48,22 +48,32 @@ class SearchDetailDialogPageView : ConstraintLayout {
         cornerKickView.setTitleText("코너킥")
         cornerKickView.setRightText(matchInfoPair.second.matchDetail.cornerKick.toString())
 
-        cardView.setLeftText("R : ${matchInfoPair.first.matchDetail.redCards}, Y : ${matchInfoPair.first.matchDetail.yellowCards}")
+//        cardView.setLeftText("R : ${matchInfoPair.first.matchDetail.redCards}, Y : ${matchInfoPair.first.matchDetail.yellowCards}")
+//        cardView.setTitleText("카드")
+//        cardView.setRightText("R : ${matchInfoPair.second.matchDetail.redCards}, Y : ${matchInfoPair.second.matchDetail.yellowCards}")
+
+        Log.v(TAG,"TEST1, ${matchInfoPair.first.matchDetail.redCards}, ${matchInfoPair.first.matchDetail.yellowCards}")
+        Log.v(TAG,"TEST2, ${matchInfoPair.second.matchDetail.redCards}, ${matchInfoPair.second.matchDetail.yellowCards}")
+        cardView.addLeftCard(matchInfoPair.first.matchDetail.redCards, matchInfoPair.first.matchDetail.yellowCards)
         cardView.setTitleText("카드")
-        cardView.setRightText("R : ${matchInfoPair.second.matchDetail.redCards}, Y : ${matchInfoPair.second.matchDetail.yellowCards}")
+        cardView.addRightCard(matchInfoPair.second.matchDetail.redCards, matchInfoPair.second.matchDetail.yellowCards)
 
         var passSuccessRate:Double = matchInfoPair.first.pass.passSuccess.toDouble() / matchInfoPair.first.pass.passTry.toDouble() * 100
         passView.setLeftText("${String.format("%.2f", passSuccessRate)}%\n${matchInfoPair.first.pass.passSuccess} / ${matchInfoPair.first.pass.passTry}")
-        passView.setTitleText("패스 성공률")
+        passView.setTitleText("패스 성공률\n시도/성공")
         passSuccessRate = matchInfoPair.second.pass.passSuccess.toDouble() / matchInfoPair.second.pass.passTry.toDouble() * 100
         passView.setRightText("${String.format("%.2f", passSuccessRate)}%\n${matchInfoPair.second.pass.passSuccess} / ${matchInfoPair.second.pass.passTry}")
 
-        goalView.setLeftText("${matchInfoPair.first.shoot.shootTotal} / ${matchInfoPair.first.shoot.effectiveShootTotal} / ${matchInfoPair.first.shoot.goalTotal}")
-        goalView.setTitleText("슛 성공률")
-        goalView.setRightText("${matchInfoPair.second.shoot.shootTotal} / ${matchInfoPair.second.shoot.effectiveShootTotal} / ${matchInfoPair.second.shoot.goalTotal}")
+        var shootSuccessRate:Double = matchInfoPair.first.shoot.goalTotal.toDouble() / matchInfoPair.first.shoot.shootTotal.toDouble() * 100
+        goalView.setLeftText("${String.format("%.2f",shootSuccessRate)}%\n${matchInfoPair.first.shoot.goalTotal} / ${matchInfoPair.first.shoot.effectiveShootTotal} / ${matchInfoPair.first.shoot.shootTotal}")
+        goalView.setTitleText("슛 성공률\n성공/유효/시도")
+        shootSuccessRate = matchInfoPair.second.shoot.goalTotal.toDouble() / matchInfoPair.second.shoot.shootTotal.toDouble() * 100
+        goalView.setRightText("${String.format("%.2f", shootSuccessRate)}%\n${matchInfoPair.second.shoot.goalTotal} / ${matchInfoPair.second.shoot.effectiveShootTotal} / ${matchInfoPair.second.shoot.shootTotal}")
 
-        defenceView.setLeftText("${matchInfoPair.first.defence.blockSuccess} / ${matchInfoPair.first.defence.blockTry}")
-        defenceView.setTitleText("수비(블락)")
-        defenceView.setRightText("${matchInfoPair.second.defence.blockSuccess} / ${matchInfoPair.second.defence.blockTry}")
+        var defenceSuccessRate:Double = matchInfoPair.first.defence.blockSuccess.toDouble() / matchInfoPair.first.defence.blockTry.toDouble() * 100
+        defenceView.setLeftText("${String.format("%.2f",defenceSuccessRate)}%\n${matchInfoPair.first.defence.blockSuccess} / ${matchInfoPair.first.defence.blockTry}")
+        defenceView.setTitleText("수비(블락)\n성공/시도")
+        defenceSuccessRate = matchInfoPair.second.defence.blockSuccess.toDouble() / matchInfoPair.second.defence.blockTry.toDouble() * 100
+        defenceView.setRightText("${String.format("%.2f", defenceSuccessRate)}%\n${matchInfoPair.second.defence.blockSuccess} / ${matchInfoPair.second.defence.blockTry}")
     }
 }
---
+++ app/src/main/java/com/example/figle_m/SearchList/SearchDetailView/SearchDetailItemCardView.kt
@@ -0,0 +1,73 @@
+package com.example.figle_m.SearchList.SearchDetailView
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.LayoutInflater
+import android.widget.ImageView
+import android.widget.LinearLayout
+import android.widget.TextView
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.db.williamchart.extensions.getDrawable
+import com.example.figle_m.R
+
+class SearchDetailItemCardView : ConstraintLayout {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initView(context)
+    }
+    fun initView(context: Context) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.cview_detail_card_info, this)
+    }
+
+    override fun onFinishInflate() {
+        super.onFinishInflate()
+    }
+
+    fun addLeftCard(redCount:Int, yellowCount: Int) {
+        val rootView = findViewById<LinearLayout>(R.id.group_Left) as LinearLayout
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        if (redCount == 0 && yellowCount == 0) {
+            val textView: TextView = inflater.inflate(R.layout.cview_detail_card_empty,rootView,false) as TextView
+            rootView.addView(textView)
+            return
+        }
+        for(i in 1..redCount) {
+            val imageView: ImageView = inflater.inflate(R.layout.cview_card,rootView,false) as ImageView
+            imageView.background = getDrawable(R.mipmap.red)
+            rootView.addView(imageView)
+        }
+
+        for(i in 1..yellowCount) {
+            val imageView: ImageView = inflater.inflate(R.layout.cview_card,rootView,false) as ImageView
+            imageView.background = getDrawable(R.mipmap.yellow)
+            rootView.addView(imageView)
+        }
+    }
+
+    fun addRightCard(redCount:Int, yellowCount:Int) {
+        val rootView = findViewById<LinearLayout>(R.id.group_Right)
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        if (redCount == 0 && yellowCount == 0) {
+            val textView: TextView = inflater.inflate(R.layout.cview_detail_card_empty,rootView,false) as TextView
+            rootView.addView(textView)
+            return
+        }
+        for(i in 1..redCount) {
+            val imageView: ImageView = inflater.inflate(R.layout.cview_card,rootView,false) as ImageView
+            imageView.background = getDrawable(R.mipmap.red)
+            rootView.addView(imageView)
+        }
+
+        for(i in 1..yellowCount) {
+            val imageView: ImageView = inflater.inflate(R.layout.cview_card,rootView,false) as ImageView
+            imageView.background = getDrawable(R.mipmap.yellow)
+            rootView.addView(imageView)
+        }
+    }
+
+    fun setTitleText(text:String) {
+        findViewById<TextView>(R.id.txt_title).text = text
+    }
+}
