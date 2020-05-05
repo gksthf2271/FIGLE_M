70dc04d5626a7aaeda317912bac245af40621753
gksthf2271 <rlagksthf33@naver.com>
2020-05-05 23:10:32 +0900

    init

--- app/src/main/java/com/khs/figle_m/utils/DisplayUtils.kt
+++ app/src/main/java/com/khs/figle_m/utils/DisplayUtils.kt
@@ -2,7 +2,11 @@ package com.khs.figle_m.utils
 
 import android.content.Context
 import android.graphics.Point
+import android.text.Spannable
+import android.text.SpannableStringBuilder
+import android.text.style.AbsoluteSizeSpan
 import android.view.WindowManager
+
 
 class DisplayUtils {
     constructor()
@@ -13,6 +17,26 @@ class DisplayUtils {
         val size = Point()
         display.getSize(size)
         return size
+    }
+
+//    ssb.setSpan(new StyleSpan(Typeface.BOLD), 0, title.length()
+//    , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); // Style
+
+//    ssb.setSpan(new ForegroundColorSpan(Color.BLUE), 0, title.length()
+//    , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); // Color
+
+//    ssb.setSpan(new AbsoluteSizeSpan(50), 0, title.length()
+//    , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); // Size
+
+//    ssb.setSpan(new UnderLineSpan(), 0, title.length()
+//    , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE); // UnderLine
+
+    open fun updateTextSize(fullStr: String, targetStr: String) : SpannableStringBuilder{
+        val ssb = SpannableStringBuilder(fullStr)
+        ssb.setSpan(
+            AbsoluteSizeSpan (30), fullStr.length - targetStr.length, fullStr.length
+            , Spannable.SPAN_EXCLUSIVE_EXCLUSIVE)
+        return ssb
     }
 
 }
