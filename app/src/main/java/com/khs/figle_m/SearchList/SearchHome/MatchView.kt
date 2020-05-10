be861b465be55ace48bd86bf04497eb980c80980
gksthf2271 <rlagksthf33@naver.com>
2020-05-10 13:15:30 +0900

    추후 업데이트 예정 소스

---
+++ app/src/main/java/com/khs/figle_m/SearchList/SearchHome/MatchView.kt
@@ -0,0 +1,53 @@
+package com.khs.figle_m.SearchList.SearchHome
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.khs.figle_m.Data.DataManager
+import com.khs.figle_m.R
+import kotlinx.android.synthetic.main.cview_match_type_view.view.*
+
+class MatchView : ConstraintLayout {
+    constructor(context: Context) : this(context, null)
+    constructor(context: Context, attrs: AttributeSet?) : this(context, attrs, 0)
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(context, attrs, defStyleAttr) {
+        initView(context)
+    }
+
+    val TAG = javaClass.name
+
+    fun initView(context: Context) {
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.cview_match_type_view, this)
+    }
+
+    fun updateView(matchType: Int) {
+        Log.v(TAG,"updateView : ${matchType}")
+        hideEmptyView()
+        when(matchType) {
+            DataManager.matchType.normalMatch.matchType -> {
+                img_icon.setImageResource(R.drawable.analytics)
+                txt_title.text = "1 ON 1\n전적 검색"
+            }
+            DataManager.matchType.coachMatch.matchType -> {
+                img_icon.setImageResource(R.drawable.strategy)
+                txt_title.text = "감독 모드\n전적 검색"
+            }
+        }
+    }
+
+    fun showEmptyView() {
+        Log.v(TAG,"showEmptyView(...)")
+        group_view.visibility = View.GONE
+        empty_view.visibility = View.VISIBLE
+    }
+
+    fun hideEmptyView() {
+        Log.v(TAG,"hideEmptyView(...)")
+        empty_view.visibility = View.GONE
+        group_view.visibility = View.VISIBLE
+    }
+}
---
+++ app/src/main/java/com/khs/figle_m/SearchList/SearchHome/SearchListFragment_ver2.kt
@@ -0,0 +1,273 @@
+package com.khs.figle_m.SearchList.SearchHome
+
+import android.annotation.SuppressLint
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.khs.figle_m.Base.BaseFragment
+import com.khs.figle_m.Data.DataManager
+import com.khs.figle_m.Home.HomeFragment
+import com.khs.figle_m.R
+import com.khs.figle_m.Response.MatchDetailResponse
+import com.khs.figle_m.Response.UserHighRankResponse
+import com.khs.figle_m.Response.UserResponse
+import com.khs.figle_m.SearchList.SearchContract
+import com.khs.figle_m.SearchList.SearchListPagerAdapter
+import com.khs.figle_m.SearchList.SearchPresenter
+import com.khs.figle_m.utils.DivisionEnum
+import com.khs.figle_m.utils.FragmentUtils
+import com.tbuonomo.viewpagerdotsindicator.WormDotsIndicator
+import kotlinx.android.synthetic.main.fragment_searchlist.*
+import kotlinx.android.synthetic.main.fragment_searchlist.btn_back
+import kotlinx.android.synthetic.main.fragment_searchlist.group_info
+import kotlinx.android.synthetic.main.fragment_searchlist.txt_Achievement_Date
+import kotlinx.android.synthetic.main.fragment_searchlist.txt_High_Rank
+import kotlinx.android.synthetic.main.fragment_searchlist.txt_Level
+import kotlinx.android.synthetic.main.fragment_searchlist.txt_MyNickName
+import kotlinx.android.synthetic.main.fragment_searchlist_ver2.*
+import okhttp3.ResponseBody
+
+
+class SearchListFragment_ver2 : BaseFragment(),
+    SearchContract.View {
+    val TAG: String = javaClass.name
+    val DEBUG: Boolean = true
+
+    open val KEY_SEARCH_USER_INFO: String = "SearchUserInfo"
+
+    lateinit var mSearchPresenter: SearchPresenter
+
+    lateinit var mOfficialGameMatchList: ArrayList<MatchDetailResponse>
+    lateinit var mCoachModeMatchList: ArrayList<MatchDetailResponse>
+
+    lateinit var mOfficialGameMatchIdList: List<String>
+    lateinit var mCoachModeMatchIdList: List<String>
+    lateinit var mSearchUserInfo: UserResponse
+
+    lateinit var mOfficialView: MatchView
+    lateinit var mCoachView: MatchView
+
+    var mCoachDivision: String = ""
+    var mNormalDivision: String = ""
+
+    var mNormalMatchResponse: UserHighRankResponse? = null
+    var mCoachMatchResponse: UserHighRankResponse? = null
+
+
+    open val KEY_MATCH_DETAIL_LIST: String = "KEY_MATCH_DETAIL_LIST"
+    override fun initPresenter() {
+        mSearchPresenter = SearchPresenter()
+    }
+
+    companion object {
+        @Volatile
+        private var instance: SearchListFragment_ver2? = null
+
+        @JvmStatic
+        fun getInstance(): SearchListFragment_ver2 =
+            instance
+                ?: synchronized(this) {
+                instance
+                    ?: SearchListFragment_ver2()
+                        .also {
+                        instance = it
+                    }
+            }
+    }
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val v: View = inflater.inflate(R.layout.fragment_searchlist_ver2, container, false)
+        return v
+    }
+
+    override fun onStart() {
+        super.onStart()
+        if (isRestartApp) return
+        mSearchPresenter!!.takeView(this)
+        initView()
+        initMyInfoData()
+        initListData()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mSearchPresenter!!.dropView()
+    }
+
+    fun initView() {
+        btn_back.setOnClickListener {
+            FragmentUtils().loadFragment(
+                HomeFragment.getInstance(),
+                R.id.fragment_container,
+                fragmentManager!!
+            )
+        }
+        mOfficialView = MatchView(context!!)
+        mCoachView = MatchView(context!!)
+        val mTeamView = TeamView(context!!)
+
+        mOfficialView.updateView(DataManager.matchType.normalMatch.matchType)
+        mCoachView.updateView(DataManager.matchType.coachMatch.matchType)
+        mTeamView.updateView(10)
+
+        viewPager_search.adapter =  SearchListPagerAdapter(context!!, mOfficialView, mCoachView)
+        viewPager_search.currentItem = 0
+        viewPager_team.adapter = SearchListPagerAdapter(context!!, mTeamView, mTeamView)
+        viewPager_team.currentItem = 0
+
+        match_indicator.setViewPager(viewPager_search)
+        team_indicator.setViewPager(viewPager_team)
+    }
+
+    fun initMyInfoData() {
+        arguments.let {
+            mSearchUserInfo = arguments!!.getParcelable<UserResponse>(KEY_SEARCH_USER_INFO)!!
+        }
+        txt_MyNickName.text = mSearchUserInfo.nickname
+        txt_Level.text = mSearchUserInfo.level
+        mSearchPresenter.getUserHighRank(mSearchUserInfo.accessId)
+    }
+
+    fun initListData() {
+        mOfficialGameMatchList = arrayListOf()
+        mCoachModeMatchList = arrayListOf()
+
+        mSearchPresenter!!.getMatchId(
+            mSearchUserInfo.accessId!!,
+            DataManager.matchType.normalMatch,
+            DataManager.getInstance().offset,
+            DataManager.getInstance().SEARCH_LIMIT
+        )
+
+        mSearchPresenter!!.getMatchId(
+            mSearchUserInfo.accessId!!,
+            DataManager.matchType.coachMatch,
+            DataManager.getInstance().offset,
+            DataManager.getInstance().SEARCH_LIMIT
+        )
+    }
+
+    override fun showLoading() {
+        Log.v(TAG, "showLoading(...)")
+        avi_loading2.visibility = View.VISIBLE
+        group_content.visibility = View.GONE
+//        group_rate.visibility = View.GONE
+        viewPager_team.visibility = View.GONE
+        viewPager_search.visibility = View.GONE
+        avi_loading2.show(false)
+    }
+
+    override fun hideLoading(isError: Boolean) {
+        Log.v(TAG, "hideLoading(...)")
+        avi_loading2.hide()
+        avi_loading2.visibility = View.GONE
+        group_content.visibility = View.VISIBLE
+//            group_rate.visibility = View.VISIBLE
+        viewPager_team.visibility = View.VISIBLE
+        viewPager_search.visibility = View.VISIBLE
+        btn_back.visibility = View.VISIBLE
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun showOfficialGameMatchIdList(userMatchIdResponse: ResponseBody?) {
+        userMatchIdResponse ?: return
+        var result: String = userMatchIdResponse.string()
+        mOfficialGameMatchIdList = arrayListOf()
+        mOfficialGameMatchIdList = result.removeSurrounding("[", "]").replace("\"", "").split(",")
+
+        if (result == null || result.isEmpty() || "[]".equals(result)) {
+            Log.v(TAG,"officialGmae is null")
+            mOfficialView.showEmptyView()
+            return
+        } else {
+            mOfficialView.hideEmptyView()
+        }
+
+        for (item in mOfficialGameMatchIdList) {
+            if (DEBUG) Log.v(TAG, "item, matchId : ${item} ")
+            mSearchPresenter.getMatchDetailList(true, item)
+        }
+    }
+
+    @SuppressLint("SetTextI18n")
+    override fun showCoachModeMatchIdList(matchDetailResponse: ResponseBody?) {
+        matchDetailResponse ?: return
+        var result: String = matchDetailResponse.string()
+        mCoachModeMatchIdList = arrayListOf()
+        mCoachModeMatchIdList = result.removeSurrounding("[", "]").replace("\"", "").split(",")
+
+        if (result == null || result.isEmpty() || "[]".equals(result)) {
+            Log.v(TAG,"coachList is null")
+            mCoachView.showEmptyView()
+            return
+        } else {
+            mCoachView.hideEmptyView()
+        }
+
+        for (item in mCoachModeMatchIdList) {
+            if (DEBUG) Log.v(TAG, "coach item, matchId : ${item} ")
+            mSearchPresenter.getMatchDetailList(false, item)
+        }
+    }
+
+    override fun showOfficialGameList(searchResponse: MatchDetailResponse?) {
+        searchResponse ?: return
+        Log.v(TAG, "showOfficialGameList : ${searchResponse!!.matchId}")
+        synchronized("Lock") {
+            mOfficialGameMatchList.add(searchResponse!!)
+            mOfficialGameMatchList.sortByDescending { it.matchDate }
+        }
+
+        if(DEBUG) Log.v(TAG,"mOfficialGameMatchList size : ${mOfficialGameMatchList.size} , mOfficialGameMatchIdList size : ${mOfficialGameMatchIdList.size}")
+    }
+
+    override fun showCoachModeList(searchResponse: MatchDetailResponse?) {
+        searchResponse ?: return
+        Log.v(TAG, "showCoachModeList : ${searchResponse!!.matchId}")
+        synchronized("Lock") {
+            mCoachModeMatchList.add(searchResponse!!)
+            mCoachModeMatchList.sortByDescending { it.matchDate }
+        }
+        if(DEBUG)  Log.v(TAG,"mCoachModeMatchList size : ${mCoachModeMatchList.size} , mOfficialGameMatchIdList size : ${mCoachModeMatchList.size}")
+    }
+
+    override fun showHighRank(userHighRankResponse: List<UserHighRankResponse>) {
+        if (userHighRankResponse.isEmpty() || userHighRankResponse.size == 0) {
+            showError(SearchPresenter().ERROR_EMPTY)
+            return
+        }
+        for (item in userHighRankResponse) {
+            if (DataManager.matchType.normalMatch.matchType == item.matchType) {
+                mNormalMatchResponse = item
+            } else if (DataManager.matchType.coachMatch.matchType == item.matchType) {
+                mCoachMatchResponse = item
+            }
+        }
+
+        for (item in DivisionEnum.values()) {
+            if (mNormalMatchResponse != null && item.divisionId.equals(mNormalMatchResponse!!.division)) {
+                mNormalDivision = item.divisionName
+                txt_High_Rank.text = mNormalDivision ?: "-"
+                txt_Achievement_Date.text =
+                    mNormalMatchResponse!!.achievementDate.replace("T", " / ")
+            } else if (mCoachMatchResponse != null && item.divisionId.equals(mCoachMatchResponse!!.division)) {
+                mCoachDivision = item.divisionName
+                txt_CoachMode_High_Rank.text = mCoachDivision ?: "-"
+                txt_CoachMode_Achievement_Date.text =
+                    mCoachMatchResponse!!.achievementDate.replace("T", " / ")
+            }
+        }
+    }
+
+    override fun showError(error: Int) {
+        if (SearchPresenter().ERROR_EMPTY.equals(error)) {
+            hideLoading(true)
+        }
+    }
+}
