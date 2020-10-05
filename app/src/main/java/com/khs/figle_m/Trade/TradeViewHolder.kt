67d6e1b88605534c49e441a6b184e7bba8aed6a6
gksthf2271 <rlagksthf33@naver.com>
2020-10-05 18:05:50 +0900

    Trade UI 문구 색상 수정

--- app/src/main/java/com/khs/figle_m/Trade/TradeViewHolder.kt
+++ app/src/main/java/com/khs/figle_m/Trade/TradeViewHolder.kt
@@ -1,5 +1,6 @@
 package com.khs.figle_m.Trade
 
+import android.text.format.DateUtils
 import android.view.View
 import androidx.recyclerview.widget.RecyclerView
 import com.khs.figle_m.Common.CirclePlayerView
@@ -23,14 +24,16 @@ open class TradeSellViewHolder(itemView: View) : TradeViewHolder(itemView) {
 
 open class TradeBuyViewHolder(itemView: View) : TradeViewHolder(itemView) {
    override fun bind(item: TradeResponse) {
-      itemView.buy_txt_date.text = item.tradeDate.replace("T","\n")
+      itemView.buy_txt_date.text = com.khs.figle_m.utils.DateUtils().formatTimeString(item.tradeDateMs)
       var circlePlayerView = itemView.buy_layout_trade_player.findViewWithTag<CirclePlayerView>("com.khs.figle_m.Common.CirclePlayerView")
       circlePlayerView.updateView(item.spid.toString(), -1, false, item.grade.toInt(), -1, -1, item.imageResUrl)
       itemView.txt_player_value.text = item.value.toString() + " BP"
       if (item.tradeType == TradeHomeFragment.TradeType.sell.ordinal) {
-         itemView.txt_player_value.background = itemView.context.resources.getDrawable(R.color.red_color,null)
+         itemView.trade_type.text = "SELL"
+         itemView.trade_type.setTextColor(itemView.context.getColor(R.color.trade_red_color))
       } else {
-         itemView.txt_player_value.background = itemView.context.resources.getDrawable(R.color.blue_color,null)
+         itemView.trade_type.text = "BUY"
+         itemView.trade_type.setTextColor(itemView.context.getColor(R.color.trade_blue_color))
       }
    }
 }
