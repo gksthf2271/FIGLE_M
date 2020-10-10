45d408ae199eab97aa31c88de771c5b51cc59429
gksthf2271 <rlagksthf33@naver.com>
2020-10-10 17:35:55 +0900

    Bottom Menu init

---
+++ app/src/main/java/com/khs/figle_m/BottomMenu/BottomNavigationBar.kt
@@ -0,0 +1,25 @@
+package com.khs.figle_m.BottomMenu
+
+import android.content.Context
+import android.util.AttributeSet
+import android.util.Log
+import android.view.LayoutInflater
+import androidx.constraintlayout.widget.ConstraintLayout
+import com.khs.figle_m.R
+
+class BottomNavigationBar(context: Context?, attrs: AttributeSet?) :
+    ConstraintLayout(context, attrs) {
+    private val TAG = javaClass.name
+
+    init {
+        initView()
+    }
+
+    fun initView() {
+        Log.v(TAG, "initView")
+        val inflater = context.getSystemService(Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater
+        inflater.inflate(R.layout.cview_loading_view, this)
+    }
+
+
+}
